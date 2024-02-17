ba8a251af947ef698724833b22a9a933d2db6fb0
Kuroi_Cc <chenchengmikuro@gmail.com>
2024-02-17 11:34:26 +0900

    feat: :bug: Add error handling for invalid scent data in create_item_scent API

--- backend_server/api/scent.py
+++ backend_server/api/scent.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 import backend_server.schemas.scent as scent_schema
@@ -8,13 +8,25 @@ from backend_server.database import get_db
 router = APIRouter()
 
 
-@router.post("/item-scent")
+@router.post(
+    "/scent",
+    responses={
+        400: {
+            "description": "Invalid scent data",
+        },
+    },
+)
 def create_item_scent(
     scent_data: scent_schema.ScentApiCreate,
     db: Session = Depends(get_db),
 ):
 
     db_obj_in = scent_service.convert_api_data_to_db_data(scent_data)
+    if db_obj_in is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid scent data",
+        )
     scent_service.create_scent(db, db_obj_in)
 
     return {"message": "post scent data completed."}
