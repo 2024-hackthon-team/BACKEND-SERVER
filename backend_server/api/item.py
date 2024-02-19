cc27692287c6ea00e7934557c97459735d72e028
Kuroi_Cc <chenchengmikuro@gmail.com>
2024-02-19 15:01:22 +0900

    feat: :sparkles: Implement PATCH /item/{item_id}/scent API for registering scent to item

--- backend_server/api/item.py
+++ backend_server/api/item.py
@@ -72,23 +72,31 @@ async def read_item(
 @router.patch(
     "/item/{item_id}/scent",
     response_model=item_schema.ItemApiRead,
-    # ! Not implemented
-    summary="Not implemented",
 )
 def register_scent_to_item(
     item_id: int,
     scent_id: int,
     db: Session = Depends(get_db),
 ):
-    # TODO: implement
-    return item_schema.ItemApiRead(
-        id=item_id,
-        item_name="item_name",
-        product_label="product_label",
-        scent_id=scent_id,
-        img_url="img_url",
-        item_tags=[
-            item_schema.ItemTagApiRead(id=1, item_tag_name="tag1"),
-            item_schema.ItemTagApiRead(id=2, item_tag_name="tag2"),
-        ],
-    )
+    item: item_model.Item = db.query(item_model.Item).get(item_id)
+    if item is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Item not found",
+        )
+
+    item.scent_id = scent_id
+
+    try:
+        db.add(item)
+        db.commit()
+        db.refresh(item)
+    except IntegrityError as e:
+        if "foreign key constraint" in str(e) and "scent_id" in str(e):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Scent not found",
+            )
+        raise e
+
+    return item
